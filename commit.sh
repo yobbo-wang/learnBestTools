21e29cf034213e8b2668e941d3bea3166907d7b9
杨剑 <yobbo@YobbodeMacBook-Pro.local>
2018-04-07 23:22:42 +0800

    first commit

---
+++ commit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+git add android/*
+git add app.json
+git add index.js
+git add ios/*
+git add js/*
+git add res/*
+git add node_modules/*
+git add package.json
+git add yarn.lock
+git add README.md
+git add .babelrc
+git add .buckconfig
+git add .flowconfig
+git add .gitattributes
+git add .gitignore
+git add .watchmanconfig
+git add README.md
+git add commit.sh
+git commit -m "first commit"
+git remote add origin https://github.com/yobbo-wang/learnBestTools.git
+git push -u origin master
